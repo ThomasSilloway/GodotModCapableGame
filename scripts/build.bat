e9a59c1ca7db14966da8404d08dffd4adc4f25ac
Thomas Silloway <tcsillow@gmail.com>
2025-02-19 12:09:49 -0500

    Update build.bat to run the game after building

--- scripts/build.bat
+++ scripts/build.bat
@@ -22,10 +22,11 @@ set second=%second:~0,2%
 set datetime=%year%-%month%-%day%_%hour%-%minute%-%second%
 
 :: Define paths
-set godot_path="C:\Godot\Godot_v4.4-beta1_win64.exe\Godot_v4.4-beta1_win64.exe"
+set godot_path="C:\Godot\Godot_v4.4-beta4_win64"
 set project_path=C:\GithubRepos\GodotModCapableGame\projects\
 set export_path=C:\GithubRepos\GodotModCapableGame\build\%datetime%\
 set export_path_file=main.exe
+set run_path_file=main.console.exe
 set main_project_path=main\
 
 set export_path_mod=mods\
@@ -39,5 +40,15 @@ mkdir "%export_path%%export_path_mod%"
 :: Export projects
 %godot_path% --headless --path %project_path%%main_project_path% --export-release "Windows Desktop" %export_path%%export_path_file%
 %godot_path% --headless --path %project_path%%mod_name_00% --export-pack "Windows Desktop" %export_path%%export_path_mod%%mod_name_00%%mod_filetype%
+
+:: Check if the main executable was created successfully
+if exist "%export_path%%run_path_file%" (
+	echo Build successful. Running the game...
+	cd /d "%export_path%"
+	echo Running: "%export_path%%run_path_file%"
+	start "" "%export_path%%run_path_file%"
+) else (
+	echo Build failed. Executable not found.
+)
 
 endlocal
