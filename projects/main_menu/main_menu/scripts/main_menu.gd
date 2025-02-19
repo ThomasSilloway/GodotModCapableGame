75b6e2c76e88461919db6b2ae9fd095f32a9b6af
Thomas Silloway <tcsillow@gmail.com>
2025-02-19 16:16:07 -0500

    Main Menu - Remove settings button

--- projects/main_menu/main_menu/scripts/main_menu.gd
+++ projects/main_menu/main_menu/scripts/main_menu.gd
@@ -2,9 +2,6 @@ extends Control
 
 @onready var buttons = $VBoxContainer
 
-# TODO: Update this when we have a settings scene that can be opened both in main menu and in game
-# @onready var settings: Panel = $Settings
-
 ## This template does not supply loading screens, so you have to make one yourself
 func _play() -> void:
 	print("Play")
@@ -15,14 +12,11 @@ func _play() -> void:
 		game_scene = config_manager.config.game_scene
 	get_tree().change_scene_to_file(game_scene)
 
-## Update me to show settings
-func _settings() -> void:
-	# settings.show()
-	buttons.hide()
-
 func _quit() -> void:
 	self.get_tree().quit()
 
-# Not sure how to implement this yet, we shall see!
-func _on_settings_hidden() -> void:
+func hide_buttons() -> void:
+	buttons.hide()
+	
+func show_buttons() -> void:
 	buttons.show()
